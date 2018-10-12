81ea26354295c6081df20e2752f25ae140a7db86
HeyItsMeNobody <blomdyon@yahoo.com>
2018-10-12 15:55:02 +0200

    Actually fixed saychannel.js

--- cmds/saychannel.js
+++ cmds/saychannel.js
@@ -7,13 +7,16 @@ module.exports.run = async (client, message, messageArray,args) => {
         message.channel.send("You do not have the administrator permission, Sorry >.<");
         return;
     }
-    if (messageArray[2] === "") {
-        message.channel.send("You put nothing after the serverID to send.. So i ignored you >.<")
-    }
     try {
         const toSend = messageArray.splice(2);
         const sayMessage = toSend.join(" ");
-        client.channels.get(`${messageArray[1]}`).send(sayMessage);
+        if (sayMessage === "") {
+            message.channel.send("You put nothing after the serverID to send.. So i ignored you >.<");
+            return;
+        }
+        else{
+            client.channels.get(`${messageArray[1]}`).send(sayMessage);
+        }
     }
     catch(err) {
         message.channel.send("Uhoh! Something went wrong, Did you spell the channel ID right?");
