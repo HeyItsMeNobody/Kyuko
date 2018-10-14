86cd43500c297aad8fbf199b4cf48ce42559d72d
HeyItsMeNobody <blomdyon@yahoo.com>
2018-10-14 17:54:41 +0200

    Help command.

---
+++ cmds/help.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const config = require('../config.json');
+const client = new Discord.Client();
+
+module.exports.run = async (client, message, args) => {
+    const embed = new Discord.RichEmbed()
+    .setTitle("Help")
+    .setDescription(`Click [here](http://kyuko.nobody.life/index.html) to go to the help page!`)
+    .setColor(0xffd1e8)
+
+    message.channel.send(embed);
+}
+
+module.exports.help = {
+    name: "help"
+}
