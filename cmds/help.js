27bea55f798229e9e1d6198b8c10036a200ef691
HeyItsMeNobody <blomdyon@yahoo.com>
2018-10-20 11:58:53 +0200

    Changed help link

--- cmds/help.js
+++ cmds/help.js
@@ -5,9 +5,8 @@ const client = new Discord.Client();
 module.exports.run = async (client, message, args) => {
     const embed = new Discord.RichEmbed()
     .setTitle("Help")
-    .setDescription(`Click [here](http://kyuko.nobody.life:9999/help) to go to the help page!`)
+    .setDescription(`Click [here](http://kyuko.nobody.life:9999/commands) to go to the help page!`)
     .setColor(0xffd1e8)
-
     message.channel.send(embed);
 }
 
